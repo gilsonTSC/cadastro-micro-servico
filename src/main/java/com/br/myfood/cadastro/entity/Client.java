2f075df08fe3f2254eda1c85c161bfb2c29b44f1
Gilson Trajano <gilson.costa@dce.ufpb.br>
2020-11-03 20:37:18 -0300

    Configurando a conexão ao banco e refatorando a entity client

--- src/main/java/com/br/myfood/cadastro/entity/Client.java
+++ src/main/java/com/br/myfood/cadastro/entity/Client.java
@@ -1,6 +1,9 @@
 package com.br.myfood.cadastro.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.modelmapper.ModelMapper;
@@ -20,6 +23,9 @@ import lombok.NoArgsConstructor;
 @Table(name = "tb_client")
 public class Client {
 
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private Long id;
 	private String name;
 	private String email;
 	private String password;
